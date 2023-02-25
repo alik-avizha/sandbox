9ee8b6f870fd6afa0fb00c74558c9cd12eb4d9e8
Alik_Avizha <aleksandravizha@icloud.com>
2023-02-25 13:41:12 +0300

    add 88 ex

---
+++ freecodecamp/JS/88ex.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true,
+    },
+    {
+        "artist": "fdsf",
+        "title": "fdsf",
+        "release_year": 1832,
+        "formats": [
+            "fsd",
+            "fs"
+        ]
+    }
+
+];
